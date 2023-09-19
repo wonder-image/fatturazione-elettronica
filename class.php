416e045b2560ebc4079a8e7a19b2f5adb0ef5238
Andrea Marinoni <114833396+wonder-image@users.noreply.github.com>
2023-09-19 16:52:55 +0200

    Update class.php
    Signed-off-by: Andrea Marinoni <114833396+wonder-image@users.noreply.github.com>

--- class.php
+++ class.php
@@ -5,7 +5,7 @@
         public const TIPO_DOCUMENTO = "TD01";
         public const VALUTA = "EUR";
 
-        public const VERSIONE = 'FPA12'; # Fattura privati: FPR12 - Fattura aziende: FPA12
+        public const FORMATO_TRASMISSIONE = 'FPR12';
         public const XMLNS_DS = 'http://www.w3.org/2000/09/xmldsig#';
         public const XMLNS_P = 'http://ivaservizi.agenziaentrate.gov.it/docs/xsd/fatture/v1.2';
         public const XMLNS_XSI = 'http://www.w3.org/2001/XMLSchema-instance';
@@ -13,19 +13,19 @@
 
         public const ID_PAESE = "IT";
         public const PROGRESSIVO_INVIO = 1;
-        public const CODICE_DESTINATARIO = "000000";
+        public const CODICE_DESTINATARIO = "0000000";
 
         public $default;
         public $file;
         public $header;
         public $body;
 
-        public function __construct($TipoDocumento = self::TIPO_DOCUMENTO, $Valuta = self::VALUTA){
+        public function __construct($ProgressivoInvio = self::PROGRESSIVO_INVIO, $CodiceDestinatario = self::CODICE_DESTINATARIO){
 
             $this->file = [];
             $this->file['FatturaElettronica'] = [];
             $this->file['FatturaElettronica']['attributes'] = [
-                "versione" => self::VERSIONE,
+                "versione" => self::FORMATO_TRASMISSIONE,
                 "xmlns:ds" => self::XMLNS_DS,
                 "xmlns:p" => self::XMLNS_P,
                 "xmlns:xsi" => self::XMLNS_XSI,
@@ -37,25 +37,31 @@
             $this->header = [];
             $this->body = [];
 
-            $this->default['TipoDocumento'] = $TipoDocumento;
-            $this->default['Divisa'] = $Valuta;
+            $this->default['TipoDocumento'] = self::TIPO_DOCUMENTO;
+            $this->default['Divisa'] = self::VALUTA;
             $this->default['Data'] = date("Y-m-d");
             
             $this->default['IdPaese'] = self::ID_PAESE;
-            $this->default['FormatoTrasmissione'] = self::VERSIONE;
-            $this->default['ProgressivoInvio'] = self::PROGRESSIVO_INVIO;
-            $this->default['CodiceDestinatario'] = self::CODICE_DESTINATARIO;
+            $this->default['FormatoTrasmissione'] = self::FORMATO_TRASMISSIONE;
+            $this->default['ProgressivoInvio'] = $ProgressivoInvio;
+            $this->default['CodiceDestinatario'] = $CodiceDestinatario;
+            $this->default['NumeroLinea'] = 1;
 
         }
 
         /**  @param string $IdPaese Sigla ISO 3166-1 alpha-2 code */ 
-        public function IdPaese($IdPaese = "IT") { $this->default['IdPaese'] = strtoupper($IdPaese); }
+        public function IdPaese($IdPaese = self::ID_PAESE) { $this->default['IdPaese'] = strtoupper($IdPaese); }
 
         /**  @param string $codice Alfanumerico Max 10 caratteri */ 
         public function ProgressivoInvio($codice = self::PROGRESSIVO_INVIO) { $this->default['ProgressivoInvio'] = $codice; }
 
         /**  @param string $codiceTrasmissione FPA12 | FPR12 */ 
-        public function FormatoTrasmissione($codiceTrasmissione = self::VERSIONE) { $this->default['FormatoTrasmissione'] = strtoupper($codiceTrasmissione); }
+        public function FormatoTrasmissione($FormatoTrasmissione = self::FORMATO_TRASMISSIONE) { $this->default['FormatoTrasmissione'] = strtoupper($FormatoTrasmissione); }
+        
+        public function CodiceDestinatario($CodiceDestinatario = self::CODICE_DESTINATARIO) { $this->default['CodiceDestinatario'] = strtoupper($CodiceDestinatario); }
+
+        /** @param string $Valuta ISO 4217 alpha-3:2001 */
+        public function Valuta($Valuta = self::VALUTA) { $this->default['Divisa'] = strtoupper($Valuta); }
 
         // FATTURAELETTRONICAHEADER
 
@@ -198,7 +204,7 @@
 
             }
 
-            public function DatiTrasmissione($IdCodice, $CodiceDestinatario, $PECDestinatario = '', $Telefono = '', $Email = '') {
+            public function DatiTrasmissione($IdCodice, $CodiceDestinatario = '', $PECDestinatario = '', $Telefono = '', $Email = '') {
 
                 $DatiTrasmissione = [];
                 $DatiTrasmissione['IdTrasmittente'] = $this->IdTrasmittente($IdCodice);
@@ -289,48 +295,234 @@
 
                 $DatiGeneraliDocumento['Numero'] = $this->default['ProgressivoInvio'];
 
-                
                 return $DatiGeneraliDocumento;
+
+            }
+
+            public function DatiBeniServizi() {
+
+                $this->body['DatiBeniServizi'] = [];
+                $this->body['DatiBeniServizi']['DettaglioLinee'] = [];
+                $this->body['DatiBeniServizi']['DettaglioLinee']['array'] = true;
+                $this->body['DatiBeniServizi']['DettaglioLinee']['child'] = [];
+
+            }
+
+            public function Sconto($Importo, $Simbolo = "valuta") {
+
+                $ScontoMaggiorazione = [];
+
+                $ScontoMaggiorazione['Tipo'] = 'SC';
+
+                $Importo = number_format($Importo, 2, '.', '');
+
+                if ($Simbolo == 'valuta') {
+                    $ScontoMaggiorazione['Importo'] = $Importo;
+                } elseif ($Simbolo == 'percentuale') {
+                    $ScontoMaggiorazione['Percentuale'] = $Importo;
+                }
+
+                return $ScontoMaggiorazione;
+
+            }
+
+            public function Maggiorazione($Importo, $Simbolo = "valuta") {
+
+                $ScontoMaggiorazione = [];
+
+                $ScontoMaggiorazione['Tipo'] = 'MG';
+
+                $Importo = number_format($Importo, 2, '.', '');
+
+                if ($Simbolo == 'valuta') {
+                    $ScontoMaggiorazione['Importo'] = $Importo;
+                } elseif ($Simbolo == 'percentuale') {
+                    $ScontoMaggiorazione['Percentuale'] = $Importo;
+                }
+
+                return $ScontoMaggiorazione;
+
+            }
+
+            public function Linea($Descrizione, $AliquotaIVA, $Natura, $Quantita, $PrezzoUnitario, $PrezzoTotale, $ScontoMaggiorazione = []) {
+
+                $Linea = [];
+
+                $Linea['NumeroLinea'] = $this->default['NumeroLinea'];
+                $Linea['Descrizione'] = $Descrizione;
+                $Linea['AliquotaIVA'] = number_format($AliquotaIVA, 2, '.', '');
+
+                if (!empty($Natura)) { $Linea['Natura'] = $Natura; }
+
+                $Linea['Quantita'] = number_format($Quantita, 2, '.', '');
+                $Linea['PrezzoUnitario'] = number_format($PrezzoUnitario, 2, '.', '');
+                $Linea['PrezzoTotale'] = number_format($PrezzoTotale, 2, '.', '');
+
+                if (!empty($ScontoMaggiorazione)) { $Linea['ScontoMaggiorazione'] = $ScontoMaggiorazione; }
+
+                $this->default['NumeroLinea']++;
+
+                array_push($this->body['DatiBeniServizi']['DettaglioLinee']['child'], $Linea);
 
             }
 
         // 
 
-        public function export($type = 'array', $action = false) {
+        // Esportazione
+            private function arrayToXml($ARRAY, $FILENAME = null, $VERSION = "1.0", $ENCODING = "UTF-8") {
 
-            # $action = print | inline | download
-
-            $this->file['FatturaElettronica']['child']['FatturaElettronicaHeader'] = $this->header;
-            $this->file['FatturaElettronica']['child']['FatturaElettronicaBody'] = $this->body;
-
-            if ($type == 'array') {
-
-                if ($action != false) {
-                    if ($action == "inline") {
-                        prettyPrint($this->file);
-                    }
+                $XML = new DOMDocument($VERSION, $ENCODING);
+        
+                $XML->preserveWhiteSpace = true;
+                $XML->formatOutput = true;
+        
+                $XML = $this->createXML($XML, $ARRAY);
+        
+                if ($FILENAME == null) {
+                    
+                    return $XML->saveXML();
+        
                 } else {
-                    return $this->file;
+        
+                    header("Content-Type: text/xml");
+                    header("Content-Disposition: attachment; filename=\"$FILENAME.xml\"");
+                    header("Pragma: no-cache");
+                    header("Expires: 0");
+        
+                    echo $XML->saveXML();
+        
                 }
+        
+            }
 
-            } elseif ($type = 'xml') {
+            private function createXml($XML, $CHILD, $PARENT = null) {
 
-                $XML = arrayToXml($this->file);
-
-                if ($action != false) {
-                    if ($action == "inline") {
-                        header("Content-Type: text/xml");
-                        echo $XML;
-                    } elseif ($action == "download") {
-                        # Scarica file
+                if ($PARENT == null) {
+                    $PARENT = $XML;
+                }
+                
+                foreach ($CHILD as $NAME => $VALUE) {
+                    
+                    if (is_array($VALUE)) {
+        
+                        $elementValue = isset($VALUE['value']) ? $VALUE['value'] : "";
+                        $elementAttributes = isset($VALUE['attributes']) ? $VALUE['attributes'] : "";
+                        $elementChild = isset($VALUE['child']) ? $VALUE['child'] : "";
+                        $elementArray = ($VALUE['array'] == true) ? true : false;
+                        
+                        if (!$elementArray) {
+        
+                            $element = $XML->createElement($NAME, $elementValue);
+        
+                            if (is_array($elementAttributes)) {
+                                foreach ($elementAttributes as $attribute => $value) { $element->setAttribute($attribute, $value); }
+                            }
+        
+                            $PARENT = $PARENT->appendChild($element);
+        
+                            if (is_array($elementChild)) {
+                                foreach ($elementChild as $childName => $childValue) { 
+        
+                                    $child = [];
+                                    $child[$childName] = $childValue;
+        
+                                    if (is_array($childValue)) {
+                                        $XML = $this->createXml($XML, $child, $PARENT); 
+                                    } else {
+                                        $element = $XML->createElement($NAME, $childValue);
+                                        $PARENT->appendChild($element);
+                                    }
+        
+                                }
+                            }
+        
+                        } else {
+        
+                            foreach ($elementChild as $key => $childValue) {
+        
+                                $child = [];
+                                $child[$NAME] = $childValue;
+        
+                                if (is_array($childValue)) {
+                                    $XML = $this->createXml($XML, $child, $PARENT); 
+                                } else {
+                                    $element = $XML->createElement($NAME, $childValue);
+                                    $PARENT->appendChild($element);
+                                }
+        
+                            } 
+        
+                        }
+        
+                        if (empty($elementValue) && empty($elementAttributes) && empty($elementChild)) {
+                            foreach ($VALUE as $childName => $childValue) { 
+        
+                                $child = [];
+                                $child[$childName] = $childValue;
+        
+                                if (is_array($childValue)) {
+                                    $XML = $this->createXml($XML, $child, $PARENT); 
+                                } else {
+                                    $element = $XML->createElement($childName, $childValue);
+                                    $PARENT->appendChild($element);
+                                }
+        
+                            }
+                        } 
+        
+                    } else {
+        
+                        $element = $XML->createElement($NAME, $VALUE);
+                        $PARENT->appendChild($element);
+        
                     }
-                } else {
-                    return $XML;
+                    
                 }
+        
+                return $XML;
+        
+            }
+
+            private function prettyPrint($array) {
+
+                print("<pre>".print_r($array,true)."</pre>");
 
             }
 
-        }
+            public function export($type = 'array', $action = false) {
+
+                # $action = print | inline | download
+
+                $this->file['FatturaElettronica']['child']['FatturaElettronicaHeader'] = $this->header;
+                $this->file['FatturaElettronica']['child']['FatturaElettronicaBody'] = $this->body;
+
+                if ($type == 'array') {
+
+                    if ($action != false) {
+                        if ($action == "inline") {
+                            $this->prettyPrint($this->file);
+                        }
+                    } else {
+                        return $this->file;
+                    }
+
+                } elseif ($type = 'xml') {
+
+                    if ($action != false) {
+                        if ($action == "inline") {
+                            header("Content-Type: text/xml");
+                            echo $this->arrayToXml($this->file);
+                        } elseif ($action == "download") {
+                            $this->arrayToXml($this->file, 'Fattura '.$this->default['ProgressivoInvio']);
+                        }
+                    } else {
+                        return $this->arrayToXml($this->file);
+                    }
+
+                }
+
+            }
+        // 
 
     }
 
